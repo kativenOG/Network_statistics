c5781807ce1d64b8d8c263bc12bcbb5f6374eee3
Mangrella Andrea <mangrella.andrea@gmail.com>
2023-09-14 18:25:23 +0200

    Added most of the function for monday presentation.

--- project_1/spectral_clustering_test.py
+++ project_1/spectral_clustering_test.py
@@ -2,20 +2,22 @@ import networkx as nx
 import numpy as np 
 from sklearn.cluster import SpectralClustering,KMeans
 from sklearn import metrics
+import random 
 import os 
 
 N_INIT = 100 
 
-def applyKMeans(n_classes,adj_mat,n_init):
+def applyKMeans(n_classes,adj_mat,gt,n_init):
     """
     Another clustering technique for comparison !!
     """
+    print(f"KMeans Clustering used for comparison !!") 
     kmeans = KMeans(n_classes,n_init=n_init)
     kmeans.fit(adj_mat)
-    print(f"KMeans Clustering used for comparison !!") 
-    return kmeans.labels_ 
+    scores = getScores(kmeans.labels_,gt,verbose = True)
+    return kmeans.labels_,scores 
  
-def applySpectral(n_classes,adj_mat,assign_labels="kmeans",n_init=100):
+def applySpectral(n_classes,adj_mat,gt,assign_labels="kmeans",n_init=100):
     """
      Params:
      - 'precomputed' means we are using our own affinity Matrix when we call fit 
@@ -32,8 +34,9 @@ def applySpectral(n_classes,adj_mat,assign_labels="kmeans",n_init=100):
     else: sc = SpectralClustering(n_classes, affinity='precomputed', assign_labels="cluster_qr") 
 
     sc.fit(adj_mat)
+    scores = getScores(sc.labels_,gt,verbose = True)
 
-    return sc.labels_ 
+    return sc.labels_,scores
     
 
 def getScores(labels,gt,verbose = False):
@@ -49,8 +52,7 @@ def getScores(labels,gt,verbose = False):
         print(f"\tRand Score: {ri:.4}\n\tAdjusted Rand Score: {ari:.4}\n")
     return tuple([ri,ari]),tuple([mi,ami])
 
-
-def karateClubGeneration():
+def karateClubGenerator():
     print("KARATE CLUB GRAPH")
     G = nx.karate_club_graph()
     adj_mat = nx.to_numpy_array(G) # Return the graph adjacency matrix as a NumPy matrix.
@@ -58,34 +60,62 @@ def karateClubGeneration():
     len_classes = len(set(gt))
     return adj_mat,len_classes,gt 
 
-def sparseERGraphGeneration(n=1000,p=0.03):
-    print("SPARSE ERDOS RENYI GRAPH:")
-    G = nx.fast_gnp_random_graph(n,p)
+def SBMGenerator(sizes,p,sparse=False):
+    print(f"{'SPARSE' if sparse else ''} STOCHASTIC BLOCK MODEL GRAPH:")
+    G = nx.stochastic_block_model(sizes,p=p,sparse=sparse)
     adj_mat = nx.to_numpy_array(G) # Return the graph adjacency matrix as a NumPy matrix.
-    gt = [G.nodes[node]["club"] for node in G.nodes()]
     len_classes = len(set(gt))
-    return adj_mat,len_classes,gt 
-
-def ERGraphGeneration(n=1000,p=0.2):
-    print("ERDOS RENYI GRAPH:")
-    G = nx.erdos_renyi_graph(n,p)
-    adj_mat = nx.to_numpy_array(G) # Return the graph adjacency matrix as a NumPy matrix.
-    gt = [G.nodes[node]["club"] for node in G.nodes()]
-    len_classes = len(set(gt))
-    return adj_mat,len_classes,gt 
+    return adj_mat,len_classes
 
 if __name__ == "__main__":
 
     # Karate Club 
-    adj_mat,len_classes,gt = karateClubGeneration()
-    labels = applySpectral(len_classes,adj_mat,n_init=N_INIT)
-    ri, mi = getScores(labels=labels,gt=gt,verbose=True)
-    labels = applySpectral(len_classes,adj_mat,assign_labels="discretize")
-    ri, mi = getScores(labels=labels,gt=gt,verbose=True)
-    labels = applySpectral(len_classes,adj_mat,assign_labels="cluster_qr")
-    ri, mi = getScores(labels=labels,gt=gt,verbose=True)
-    labels = applyKMeans(len_classes,adj_mat,n_init=N_INIT)
-    ri, mi = getScores(labels=labels,gt=gt,verbose=True) 
+    adj_mat,len_classes,gt = karateClubGenerator()
+    kcscores = [] 
+    kcscores.append(applySpectral(len_classes,adj_mat,gt,n_init=N_INIT))
+    kcscores.append(applySpectral(len_classes,adj_mat,gt,assign_labels="discretize")[1])
+    kcscores.append(applySpectral(len_classes,adj_mat,gt,assign_labels="cluster_qr")[1])
+    kcscores.append(applyKMeans(len_classes,adj_mat,gt,N_INIT)[1])
 
-    
+    # SBM: 
+    # Variables: 
+    n_classes = 10
+    N1,N2 = 1000,10000
+    sizes1,sizes2 = [N1//n_classes]*n_classes,[N2//n_classes]*n_classes
+    gt1,gt2 = np.array([[val]*(N1//n_classes) for val in range(10)]), np.array([[val]*(N2//n_classes) for val in range(10)])
+    gt1,gt2 = gt1.flatten(),gt2.flatten()
+    # P defines if our matrix is dense or not
+    dense_p = np.array([random.uniform(0,1)]*(n_classes*n_classes))
+    dense_p = np.reshape(dense_p,(n_classes,n_classes))
+    sparse_p1,sparse_p2 = np.array([random.uniform(0,N1)/N1] for _ in range(N1*N1)),np.array([random.uniform(0,N2)/N2] for _ in range(n_classes*n_classes))
+    sparse_p1,sparse_p2 =  np.reshape(sparse_p1,(n_classes,n_classes)), np.reshape(sparse_p2,(n_classes,n_classes))
+
+    # Dense with N1
+    adj_mat,len_classes = SBMGenerator(sizes1,dense_p)
+    sbmscores_dense1 = [] 
+    sbmscores_dense1.append(applySpectral(len_classes,adj_mat,gt1,n_init=N_INIT))
+    sbmscores_dense1.append(applySpectral(len_classes,adj_mat,gt1,assign_labels="discretize")[1])
+    sbmscores_dense1.append(applySpectral(len_classes,adj_mat,gt1,assign_labels="cluster_qr")[1])
+    sbmscores_dense1.append(applyKMeans(len_classes,adj_mat,gt1,N_INIT)[1]) 
+    # Dense with N2
+    adj_mat,len_classes = SBMGenerator(sizes2,dense_p)
+    sbmscores_dense2 = [] 
+    sbmscores_dense2.append(applySpectral(len_classes,adj_mat,gt2,n_init=N_INIT))
+    sbmscores_dense2.append(applySpectral(len_classes,adj_mat,gt2,assign_labels="discretize")[1])
+    sbmscores_dense2.append(applySpectral(len_classes,adj_mat,gt2,assign_labels="cluster_qr")[1])
+    sbmscores_dense2.append(applyKMeans(len_classes,adj_mat,gt2,N_INIT)[1]) 
+    # Sparse with N1
+    adj_mat,len_classes = SBMGenerator(sizes1,sparse_p1)
+    sbmscores_sparse1 = [] 
+    sbmscores_sparse1.append(applySpectral(len_classes,adj_mat,gt1,n_init=N_INIT))
+    sbmscores_sparse1.append(applySpectral(len_classes,adj_mat,gt1,assign_labels="discretize")[1])
+    sbmscores_sparse1.append(applySpectral(len_classes,adj_mat,gt1,assign_labels="cluster_qr")[1])
+    sbmscores_sparse1.append(applyKMeans(len_classes,adj_mat,gt1,N_INIT)[1]) 
+    # Sparse with N2
+    adj_mat,len_classes = SBMGenerator(sizes2,sparse_p2)
+    sbmscores_sparse2 = [] 
+    sbmscores_sparse2.append(applySpectral(len_classes,adj_mat,gt2,n_init=N_INIT))
+    sbmscores_sparse2.append(applySpectral(len_classes,adj_mat,gt2,assign_labels="discretize")[1])
+    sbmscores_sparse2.append(applySpectral(len_classes,adj_mat,gt2,assign_labels="cluster_qr")[1])
+    sbmscores_sparse2.append(applyKMeans(len_classes,adj_mat,gt2,N_INIT)[1]) 
 
