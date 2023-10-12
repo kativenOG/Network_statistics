3dbb2b84d705b4473cebc43db3632f9821d7d055
Maarten van Sluijs <72663441+MaartenVanSluijs@users.noreply.github.com>
2023-10-12 14:05:17 +0200

    windows branch

--- project_2/lib/utils.py
+++ project_2/lib/utils.py
@@ -3,21 +3,23 @@ import numpy as np
 import os,shutil,subprocess,random
 
 def jazz_generator(k=10):
-    """
-    Download the dataset and return it as a list of k folds 
-    """
-    # Get rid of old repositories if present (just to be sure)
-    dir1,dir2  =  os.path.join(os.getcwd(),"{}"),os.path.join(os.getcwd(),"jazz.net")
-    if os.path.isdir(dir1): shutil.rmtree(dir1)
-    if os.path.isdir(dir2): shutil.rmtree(dir2)
+    # """
+    # Download the dataset and return it as a list of k folds 
+    # """
+    # # Get rid of old repositories if present (just to be sure)
+    # dir1,dir2  =  os.path.join(os.getcwd(),"{}"),os.path.join(os.getcwd(),"jazz.net")
+    # if os.path.isdir(dir1): shutil.rmtree(dir1)
+    # if os.path.isdir(dir2): shutil.rmtree(dir2)
 
-    # Download the dataset and unzip it 
-    subprocess.run(["wget","--no-check-certificate","-P","{}","https://deim.urv.cat/~alexandre.arenas/data/xarxes/jazz.zip"])
-    subprocess.run(["unzip","{}/jazz.zip"])
-    # Save all the lines in the Graph
-    with open("jazz.net","r") as f: lines = f.readlines()        
-    # Get rid of the Data 
-    subprocess.run(["rm","-rf","{}","jazz.net"]) 
+    # # Download the dataset and unzip it 
+    # subprocess.run(["wget","--no-check-certificate","-P","{}","https://deim.urv.cat/~alexandre.arenas/data/xarxes/jazz.zip"])
+    # subprocess.run(["unzip","{}/jazz.zip"])
+    # # Save all the lines in the Graph
+    # with open("jazz.net","r") as f: lines = f.readlines()        
+    # # Get rid of the Data 
+    # subprocess.run(["rm","-rf","{}","jazz.net"]) 
+
+    lines = open("lib\jazz.net", "r").read().splitlines()
     
     # Transformation to edge list
     data= [list(map(lambda x: int(x),line.split())) for line in lines[3:]]
