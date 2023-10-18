18d58af132d213d5138fc8a278302a658e0e90ef
Mangrella Andrea <mangrella.andrea@gmail.com>
2023-10-18 19:53:05 +0200

    Last commit for the link prediction project

--- projects/link_prediction/lib/clique_study.py
+++ projects/link_prediction/lib/clique_study.py
@@ -4,9 +4,11 @@ from icecream import ic
 
 # Spectral sutff
 import warnings 
-from scipy.sparse.linalg import eigs
+from scipy.sparse.linalg import eigen, eigs
 from .local_similarity import sort_scores
 from numpy import linalg as LA 
+from scipy.spatial.distance import pdist
+
 
 def biggest_clique_removal(k_folds:list, n_cliques:int =1)-> tuple[nx.Graph,list]:
     G = nx.from_edgelist(np.vstack(k_folds))
@@ -32,7 +34,7 @@ def biggest_clique_removal(k_folds:list, n_cliques:int =1)-> tuple[nx.Graph,list
     return nx.from_edgelist(correct_set),clique_edge_list
 
     
-def spectral_scores(G: nx.Graph,n_class:int =20)-> np.ndarray:
+def spectral_scores(G: nx.Graph,n_class:int =20,eigen_gap:int=-1)-> np.ndarray:
     # So warnings wont bother us :^) 
     warnings.filterwarnings("ignore") 
 
@@ -46,23 +48,26 @@ def spectral_scores(G: nx.Graph,n_class:int =20)-> np.ndarray:
     vals_disjoint, vecs_disjoint = np.real(vals_disjoint), np.real(vecs_disjoint) # Remove the immaginary part   
 
     # Eigen Gap Prompt:
-    print("Apply eigen Gap: ".upper())
-    print("Eigen Values:")
-    print("\n".join(list(map(lambda x : f"-{x[0]}: {x[1]} ",enumerate(vals_disjoint.flatten())))))
-    slice_line = ""
-    while not slice_line.isdigit(): slice_line = input("Insert the line where the eigen values need to be sliced: ")
-    slice_line = int(slice_line)
-    vecs_disjoint = vecs_disjoint[:,:slice_line]
+    if eigen_gap == -1:
+        print("Apply eigen Gap: ".upper())
+        print("Eigen Values:")
+        print("\n".join(list(map(lambda x : f"-{x[0]}: {x[1]} ",enumerate(vals_disjoint.flatten())))))
+        slice_line = ""
+        while not slice_line.isdigit(): slice_line = input("Insert the line where the eigen values need to be sliced: ")
+        slice_line = int(slice_line)
+        vecs_disjoint = vecs_disjoint[:,:slice_line]
+    else:
+        vecs_disjoint = vecs_disjoint[:,:eigen_gap] 
 
     # Return the eigen vectors that will work as a score for each ndoe :^)  
     return vecs_disjoint 
 
-def spectral_similarity(G: nx.Graph,n_class:int =20,reverse:bool = False)-> list[tuple[int,int,float]]:
+def spectral_similarity(G: nx.Graph,n_class:int =20,reverse:bool = False,eigen_gap:int=-1)-> list[tuple[int,int,float]]:
     # Maps for the two node numberings spectral_scores/without_clique
     mapping = { node:i for i,node in  enumerate(G.nodes())}
 
     # Get the spectral scores and all the non-edges
-    ss = spectral_scores(G,n_class)
+    ss = spectral_scores(G,n_class,eigen_gap)
 
     # Normalize the eigen vectors values 
     # for i in range(len(ss)): ss[i,:]= ss[i,:] / np.sum(ss[i,:])
@@ -70,15 +75,25 @@ def spectral_similarity(G: nx.Graph,n_class:int =20,reverse:bool = False)-> list
     # Get all the non-edges
     non_edges = nx.non_edges(G)
     
-    def formula(x: np.ndarray,y: np.ndarray)->float:
-        # cn = len(list(nx.common_neighbors(G,x,y)))
+    # Average Distance used to normalize the value 
+    avg_dist = np.mean(pdist(ss).flatten())
+
+    def formula(x: np.ndarray,y: np.ndarray):
+        cn = len(list(nx.common_neighbors(G,x,y)))
+        norm = LA.norm(x-y)
+        # return cosine_similarity
+        
+        similarity = cn/norm
+        return similarity
+
+    def old_formula(x: np.ndarray,y: np.ndarray)->float:
+        cn = len(list(nx.common_neighbors(G,x,y)))
         norm_x,norm_y = LA.norm(x),LA.norm(y)
         cosine_similarity = np.dot(ss[mapping[x],:],ss[mapping[y],:])/(norm_x*norm_y)
-        return cosine_similarity
         return cn*cosine_similarity
-
+     
     # Calculate the Scores 
-    spectral_score = (tuple([u,v,formula(u,v)]) for u,v in non_edges)
+    spectral_score = (tuple([u,v,formula(u,v)*avg_dist]) for u,v in non_edges)
     return sort_scores(spectral_score,reverse=reverse) 
 
 # For testing functions 
